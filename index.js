ae2085c88ca122bdd98f446903ca167f0631e195
icejuk <icejuk@gmail.com>
2020-02-20 01:15:05 +0700

    Remove unused

--- index.js
+++ index.js
@@ -60,31 +60,3 @@ function reParent(input) {
 server.start(function () {
 	console.log('Hapi running at: ', server.info.uri);
 });
-
-// const init = async () => {
-
-//     const server = Hapi.server({
-//         port: 3000,
-//         host: 'localhost'
-//     });
-
-//     server.route({
-//         method: 'GET',
-//         path: '/',
-//         handler: (request, h) => {
-
-//             return 'Hello World!';
-//         }
-//     });
-
-//     await server.start();
-//     console.log('Server running on %s', server.info.uri);
-// };
-
-// process.on('unhandledRejection', (err) => {
-
-//     console.log(err);
-//     process.exit(1);
-// });
-
-// init();
