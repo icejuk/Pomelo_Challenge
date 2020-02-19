4a8ba67c4928c1a42049791970ef02a332d541a2
icejuk <icejuk@gmail.com>
2020-02-20 00:46:01 +0700

    Create API, SearchAPI

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-'use strict';
-console.log('test');
+
 const Hapi = require('hapi');
-var server = new Hapi.Server();
+var path = require('path');
+
+const server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
-    port: 5555
+	host: 'localhost',
+	port: 8888
 });
 
 server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (request, reply) {
-        reply('<h1>Hello Hapi :)</h1');
-    }
+	method: 'GET',
+	path: '/',
+	handler: function (request, reply) {
+			reply.file('./index.html');
+	}
 });
+
+server.route({
+	method: 'POST',
+	path: '/reParent',
+	handler: function (request, reply) {
+		const payload = request.payload;
+		reply(reParent(payload.data));
+	}
+});
+
+function reParent(input) {
+	const children = [];
+	let parentId = null;
+	for (const key in input) {
+		const element = input[key];
+		for (const child of element) {
+			if (child.parent_id == null)
+				parentId = child.id
+			children.push(child);
+		};
+	}
+	children.sort((a, b) => (a.level > b.level) ? -1 : 1)
+
+	const result = [];
+	for (const child of children) {
+		if (!result[child.parent_id]) {
+			result[child.parent_id] = [];
+		}
+		if (!result[child.id]) {
+			result[child.id] = child;
+		}
+		else {
+			const childrenOfId = result[child.id];
+			result[child.id] = child;
+			result[child.id]['children'] = childrenOfId.reverse();
+		}
+
+		result[child.parent_id].push(result[child.id])
+	}
+	return [result[parentId]];
+}
+
 server.start(function () {
-    console.log('Hapi running at: ', server.info.uri);
+	console.log('Hapi running at: ', server.info.uri);
 });
 
 // const init = async () => {
